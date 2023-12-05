9c5d0e8e236e20581e7c7338d3f66112ffb16455
Fouad Mokhtari <fouad.mokhtari@study.hs-duesseldorf.de>
2023-12-05 22:19:14 +0100

    Fundament der Funktion in die Datei eingefügt

--- fairsharer/fairsharer.py
+++ fairsharer/fairsharer.py
@@ -1,1 +1,24 @@
 
+
+def fair_sharer(values, num_iterations, share=0.1):
+    """Runs num_iterations.
+    In each iteration the highest value in values gives a fraction (share)
+    to both the left and right neighbor. Das am weitesten links liegende Feld gilt 
+    als Nachbar des am weitesten rechts liegenden Feldes
+    
+    Examples:
+    fair_sharer([0, 1000, 800, 0], 1) --> [100, 800, 900, 0]
+    fair_sharer([0, 1000, 800, 0], 2) --> [100, 890, 720, 90]
+    
+    Args
+    values:
+        1D array of values (list or numpy array)
+
+    num_iteration:
+        Integer to set the number of iterations
+    """
+    
+    
+
+
+    return values_new
