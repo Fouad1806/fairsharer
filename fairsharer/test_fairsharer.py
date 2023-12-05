ec22b6985d890c9106af58755083562881ac4ae5
Fouad1806 <61727632+Fouad1806@users.noreply.github.com>
2023-12-05 22:52:37 +0100

    Update test_fairsharer.py

--- fairsharer/test_fairsharer.py
+++ fairsharer/test_fairsharer.py
@@ -1,1 +1,7 @@
+from fairsharer import fair_sharer
 
+def test_fair_sharer():
+    assert fair_sharer([0, 1000, 800, 0], 1) == [100, 800, 900, 0]
+    assert fair_sharer([0, 1000, 800, 0], 2) == [100, 890, 720, 90]
+    assert fair_sharer([700, 1000, 800, 0], 1) == [800, 890, 720, 90]
+    
